1fad5514a993d9672c9e0859d194ed59d9fc92f1
Manas Tare <111227859+ManasTare@users.noreply.github.com>
2025-11-29 19:46:34 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,14 @@
-alert("Hello")
+//Original Random Function
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getLink() {
+    return "https://www.pokemon.com/us/pokedex/" + getRandomInt(999);
+}
+function callme() {
+    window.open(getLink())
+}
+function tool() {
+    window.open(getLink(), "_blank", 'height=1000,width=150')
+}
